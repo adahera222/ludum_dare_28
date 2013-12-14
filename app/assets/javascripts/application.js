0a519732b272e0ada596a45c239d439baa0e21ab
arrogantgamer <arrogant.gamer@gmail.com>
2013-12-14 06:49:48 +0100

    Added Quintus libraries to vendor

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,4 +10,12 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
+//= require quintus
+//= require quintus_sprites
+//= require quintus_input
+//= require quintus_2d
+//= require quintus_scenes
+//= require jquery_ujs
+//= require turbolinks
 //= require_tree .
